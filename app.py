5399c5ce5a61dc6e764c7f46c317590f39589345
sudarshan <suddu680@gmail.com>
2025-01-15 10:00:43 +0530

    bug

--- app.py
+++ app.py
@@ -8,41 +8,51 @@ from langchain_community.vectorstores.chroma import Chroma
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import openai
+from fastapi.middleware.cors import CORSMiddleware
 
 
 load_dotenv()
 
 embedding_function = OpenAIEmbeddings()
 
-docs = [
-    Document(
-        page_content="the dog loves to eat pizza", metadata={"source": "animal.txt"}
-    ),
-    Document(
-        page_content="the cat loves to eat lasagna", metadata={"source": "animal.txt"}
-    ),
-]
 
-db = Chroma.from_documents(docs, embedding_function)
-retriever = db.as_retriever()
+# docs = [
+#     Document(
+#         page_content="the dog loves to eat pizza", metadata={"source": "animal.txt"}
+#     ),
+#     Document(
+#         page_content="the cat loves to eat lasagna", metadata={"source": "animal.txt"}
+#     ),
+# ]
+
+# db = Chroma.from_documents(docs, embedding_function)
+# retriever = db.as_retriever()
 
 
-template = """Answer the question based only on the following context:
-{context}
+template = """Answer the question:
 
 Question: {question}
 """
 prompt = ChatPromptTemplate.from_template(template)
-model = ChatOpenAI(temperature=0, streaming=True)
+model = ChatOpenAI(model="gpt-4o-mini",temperature=0, streaming=True)
 
 retrieval_chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
+    {"question": RunnablePassthrough()}
     | prompt
     | model
     | StrOutputParser()
 )
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace '*' with specific origins in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 async def generate_chat_responses(message):
     async for chunk in retrieval_chain.astream(message):
